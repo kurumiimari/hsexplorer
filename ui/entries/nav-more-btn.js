fac092eba6d69a1980f937fec5a32537c9a4ebf7
Chi Kei Chan <chikeichan@gmail.com>
2021-10-23 00:56:08 +0800

    add friends link

---
+++ ui/entries/nav-more-btn.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NavMoreButton from "../components/NavMoreButton";
+
+ReactDOM.render(
+  <NavMoreButton />,
+  document.getElementById('nav-more-btn-root'),
+);
